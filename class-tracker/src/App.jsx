cea961653d044d64f60ed14608bc56cb18dd8b17
shubham555-55 <shubhammishra23082004@gmail.com>
2026-09-16 12:28:27 +0530

    Update code in main branch

--- class-tracker/src/App.jsx
+++ class-tracker/src/App.jsx
@@ -2,10 +2,23 @@ import ClassCard from "./Components/ClassCard";
 function App() {
  return (
     <div>
+      <hr />
+      <hr />
+      <hr />
+      <hr />
       <h1>Class Details </h1>
+      <h1>-</h1>
+      <hr />
+      <hr />
       < ClassCard lab="Skill-lab" room="D-101" time="10:00 AM" ETime="12:00 AM" teacher="Rajeev sir"/>
+      <hr />
+      <hr />
       < ClassCard lab="Daa-Lab" room="D-105" time="11:00 AM"  ETime="01:00 PM" teacher="Ajay  sir"/>
+       <hr />
+      <hr />
        < ClassCard lab="Mern-lab" room="D-202" time="9:00 AM" ETime="11:00 PM" teacher="Lucky sir"/>
+      <hr />
+      <hr />
       < ClassCard lab="Physics-lab" room="D-112" time="7:00 AM" ETime="09:00 AM" teacher="Mohit sir"/>
     </div>
 )}
